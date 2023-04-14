78286797f8da396d5ee3a59054accf521cc7ae86
donlHenard <pastor.git101@gmail.com>
2023-04-14 15:42:22 +0800

    Add event to operator buttons and fn operation
    Removed the clear function and add it to fn operation with new code that
    make all variables undefined. And for operator string except for = will
    assign the display text to first num and all the click operator to
    let operator. The equal string condition remain to be solved on why it
    does not work

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const divide = (a,b) => a / b;
 let firstNum;
 let operator;
 let secondNum;
+let result;
 
 const operate = (a,b,operator) => {
     switch (operator) {
@@ -39,7 +40,9 @@ for(let digit of digitBTNs){
 
 const display = (digit) => {
     let bigDisplayText = bigDisplay.innerText;
-
+    if(firstNum != undefined && operator != undefined){
+        bigDisplay.innerText = "";
+    }
     if(bigDisplayText == "0"){
         if(digit == "00"){
             bigDisplay.innerText = "0";
@@ -58,8 +61,32 @@ const display = (digit) => {
     }
 }
 
-//CLEAR BUTTON
-const clearBTN = document.querySelector(".operator.clear");
-clearBTN.addEventListener("click", () =>{
-    bigDisplay.innerText = "0";
-})
+const operatorBTN = document.querySelectorAll(".column>.operator");
+
+for(let operator of operatorBTN){
+    operator.addEventListener("click", () => {
+        operation(operator.innerText);
+    });
+}
+
+const operation = (operator_clicked) => {
+    if(operator_clicked == "C"){
+        bigDisplay.innerText = "0";
+        firstNum = undefined;
+        operator = undefined;
+        secondNum = undefined;
+    }else if(operator_clicked.match(/[+\-*/]/)){
+        firstNum = bigDisplay.innerText;
+        operator = operator_clicked;
+    }else if(operator_clicked == "="){
+        if(secondNum == undefined){
+           //none
+        }else{
+            secondNum = bigDisplay.innerText;
+            result = operate(firstNum,secondNum,operator_clicked);
+            alert(result);
+        }
+    }else{
+        alert("error in fn operation");
+    }
+}
